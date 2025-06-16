d3ebc841609308b02efb52d95ce8e449655ef866
noah-yala <noah@yala.org>
2025-06-16 12:16:05 +0800

    SignerAction, 4, abstract class SignerAction extends Keypair implements Signer

--- src/signer-external.ts
+++ src/signer-external.ts
@@ -1,5 +1,14 @@
-import {Signer} from './keypair';
+import {Keypair, Signer} from './keypair';
+import {PublicKey} from './publickey';
 
-export interface SignerAction extends Signer {
-  sign(message: Buffer): Promise<Buffer>;
+export abstract class SignerAction extends Keypair implements Signer {
+  constructor(pk: string | PublicKey) {
+    let publicKey = pk instanceof PublicKey ? pk : new PublicKey(pk);
+    super({
+      publicKey: publicKey.toBytes(),
+      secretKey: null as unknown as Uint8Array, // Secret key is not used
+    });
+  }
+
+  abstract sign(message: Buffer): Promise<Buffer>;
 }
